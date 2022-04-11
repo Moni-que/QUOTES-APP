1c81289fe2022a694d14fca5e87a09b2836613ac
moni-que <monicahjustus@gmail.com>
2022-04-11 21:08:38 +0300

    add myarr which has math.max

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,14 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote('Start writing, no matter what. The water does not flow until the faucet is turned on.', 0, 0,'Louis LAmour', 100),
-    new Quote('Get it down. Take chances. It may be bad, but it is the only way you can do anything really good.', 0, 0,'William Faulkner', 60),
-    new Quote('The first draft is just you telling yourself the story.', 0, 0,'Terry Pratchett', 50),
-    new Quote('You do not start out writing good stuff. You start out writing crap and thinking it is good stuff, and then gradually you get better at it.', 0, 0,'Octavia E. Butler', 30),
-    new Quote('Start before you are ready.', 0, 0,'Steven Pressfield', 20)
-  ]
+    new Quote('Start writing, no matter what. The water does not flow until the faucet is turned on.', 100, 0,'Louis LAmour', 100),
+    new Quote('Get it down. Take chances. It may be bad, but it is the only way you can do anything really good.', 60, 0,'William Faulkner', 60),
+    new Quote('The first draft is just you telling yourself the story.', 50, 0,'Terry Pratchett', 50),
+    new Quote('You do not start out writing good stuff. You start out writing crap and thinking it is good stuff, and then gradually you get better at it.', 30, 0,'Octavia E. Butler', 30),
+    new Quote('Start before you are ready.', 20, 0,'Steven Pressfield', 20)
+  ];
+  myarr: number[]=this.quotes.map(quote=>quote.POINTS)
+  highest= Math.max(...this.myarr)
   constructor() { }
 
   ngOnInit(): void {
